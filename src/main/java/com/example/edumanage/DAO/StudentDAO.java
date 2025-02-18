7937a202f2f2d092d5be3189f1fd6588d7e3b420
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-02-18 09:42:16 +0100

    creat first commit

---
+++ src/main/java/com/example/edumanage/DAO/StudentDAO.java
@@ -0,0 +1,4 @@
+package com.example.edumanage.DAO;
+
+public class StudentDAO {
+}
---
+++ src/main/java/com/example/edumanage/controllers/CoursServlet.java
@@ -0,0 +1,20 @@
+package com.example.edumanage.controllers;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "CoursServlet", value = "/CoursServlet")
+public class CoursServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
