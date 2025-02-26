01a002483de275b350749786103e5470effb6db1
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-02-26 11:32:45 +0100

    new commit

--- src/main/java/com/example/edumanage/controllers/Filter.java
+++ src/main/java/com/example/edumanage/controllers/Filter.java
@@ -1,20 +1,20 @@
-package com.example.edumanage.controllers;
+//package com.example.edumanage.controllers;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.annotation.*;
+//
+//import java.io.IOException;
+//
+//@WebFilter(filterName = "Filter")
+//public class Filter implements Filter {
+//    public void init(FilterConfig config) throws ServletException {
+//    }
+//
+//    public void destroy() {
+//    }
 
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.*;
-
-import java.io.IOException;
-
-@WebFilter(filterName = "Filter")
-public class Filter implements Filter {
-    public void init(FilterConfig config) throws ServletException {
-    }
-
-    public void destroy() {
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(request, response);
-    }
-}
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+//        chain.doFilter(request, response);
+//    }
+//}
